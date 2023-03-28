fbf14fdd249c02744244d0046db1f76196292e88
not a droid <68233166+Codeblocc@users.noreply.github.com>
2023-03-28 20:56:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,35 @@ import pandas as pd
 
 
 st.write("""
-# Simple Stock Price App
+# Stock Price App
 
-Shown are the stock closing price and volume of ITC.
+Shown are the stock closing prices and volume of ITC.
 
 """)
 
 tickerSymbol = 'ITC.BO'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2023-3-28')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
+
+st.write("""
+# 
+Shown are the stock closing prices and volume of TATA Motors, India.
+
+""")
+
+tickerSymbol = 'TATAMOTORS.BO'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
